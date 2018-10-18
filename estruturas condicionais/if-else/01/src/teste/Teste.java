d96412efc45074cbe309a345f02f67442da4f58d
William Trindade <williamtrindade777@gmail.com>
2018-10-17 23:37:55 -0300

    Add files via upload

---
+++ estruturas condicionais/if-else/01/src/teste/Teste.java
@@ -0,0 +1,12 @@
+package teste;
+public class Teste {
+    public static void main(String[] args) {
+        int idade = 19;
+        if(idade >= 18){
+            System.out.println("Acesso Liberado!");
+        }else{
+            System.out.println("Acesso Negado");
+        }
+    }
+    
+}
